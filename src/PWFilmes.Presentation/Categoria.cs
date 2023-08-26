065eebaa20fd9bb4850a324f3e6aa860386200e7
Aluno <aluno@etec.sp.gov.br>
2023-08-25 22:47:11 -0300

    Instalação do Radzen, criação da class Categoria e da lista de propriedades de Categorias

---
+++ src/PWFilmes.Presentation/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWFilmes.Presentation
+{
+    public class Categoria
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public string Cor { get; set; }
+    }
+}
