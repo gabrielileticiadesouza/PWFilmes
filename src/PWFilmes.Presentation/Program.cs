6fb2d08c14895a5b78389644054251f336c94013
Aluno <aluno@etec.sp.gov.br>
2023-09-18 20:45:16 -0300

    AULA 18/09/23 Método Assíncrono, HttpClient (fazer o Blazor consumir a API)

--- src/PWFilmes.Presentation/Program.cs
+++ src/PWFilmes.Presentation/Program.cs
@@ -17,7 +17,10 @@ namespace PWFilmes.Presentation
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.
+                AddScoped(sp => new HttpClient
+                  { BaseAddress =
+                    new Uri("https://localhost:44374/api") });
 
             await builder.Build().RunAsync();
         }
