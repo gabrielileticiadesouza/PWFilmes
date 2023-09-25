c21fcc15788f16c82309fa74cbf6d5eb1b45048f
Aluno <aluno@etec.sp.gov.br>
2023-09-25 20:47:38 -0300

    Atividades aula - 25/09/2023

---
+++ PWFilmes.API/Context/PWFilmesContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PWFilmes.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PWFilmes.API.Context
+{
+    public class PWFilmesContext: DbContext
+    {
+        public PWFilmesContext(DbContextOptions<PWFilmesContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categoria> CategoriaSet { get; set; }
+    }
+
+}
--- PWFilmes.Domain/Categoria.cs
+++ PWFilmes.Domain/Categoria.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PWFilmes.Domain
 {
+    [Table("Categoria")]
    public class Categoria
     {
+        [Key]
+        [Required(ErrorMessage = "O código deve ser informado!")]
         public int Codigo { get; set; }
+
+        [Required (ErrorMessage ="A descrição deve ser informada!")]
+        [MaxLength (100, ErrorMessage = "A descrição deve conter no maximo 100 caracteres!")]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage ="A cor deve ser informada!")]
+        [MaxLength (50, ErrorMessage ="A cor deve conter no máximo 50 caracteres!!")]
         public string Cor { get; set; }
     }
 }
